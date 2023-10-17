61bc0a59a94a05b7b301e2590c60b6a7a09bebc9
devesh mishra <d.mishra56603@gmail.com>
2023-10-17 12:09:33 +0530

    added divide two numbers using binary search

---
+++ DivideTwoNumbersUsingBS.cpp
@@ -0,0 +1,43 @@
+// divide 2 numbers using binary search
+#include<iostream>
+using namespace std;
+
+int solve(int dividend, int divisor){
+    int s = 0;
+    int e = abs(dividend);
+    int ans = 0;
+    int mid = s + (e-s)/2;
+    while(s <=e){
+        //perfect sol
+        if(abs(mid*divisor) == abs(dividend)){
+            ans = mid;
+        }
+        //not perfect sol
+        if(abs(mid*divisor) > abs(dividend)){
+            //left
+            e = mid -1;
+        }
+        else{
+            //ans store
+            ans = mid;
+            //right
+            s = mid +1;
+        }
+        mid = s + (e-s)/2;
+    }
+    if((dividend <0 && divisor <0) || (dividend >0 && divisor > 0)){
+        return ans;
+    }
+    else{
+        return -ans;
+    }
+}
+
+
+int main(){
+    int dividend = -42;
+    int divisor = -7;
+
+    int ans = solve(dividend, divisor);
+    cout<<"Ans is "<< ans <<endl;
+}
