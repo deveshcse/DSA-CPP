06f2276847c80d68c34d74ed6bb244942cdea594
devesh mishra <d.mishra56603@gmail.com>
2023-10-17 19:50:08 +0530

    added find odd element in array

---
+++ FindOddOccuringElement.cpp
@@ -0,0 +1,43 @@
+//find odd occuring element in an arry
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int solve(vector<int> arr){
+    int s = 0;
+    int e = arr.size()-1;
+    int mid = s +(e-s)/2;
+    while(s<=e){
+        if(s==e){
+            return mid;
+        }
+        if(mid%2 == 0){
+            if(arr[mid] == arr[mid+1]){
+                s = mid + 2;
+            }
+            else{
+                e = mid ;
+            }
+        }
+        else{
+            if(arr[mid] == arr[mid-1]){
+                s = mid +1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+        mid = s + (e-s)/2;
+        
+    }
+    return mid;
+}
+
+int main(){
+    vector<int> arr{1,1,2,2,3,3,4,4,3,600,600,4,4};
+    int ans = solve(arr);
+    cout<< "ans is "<< ans << endl;
+    cout<< "value is "<< arr[ans] <<endl;
+    return 0;
+
+}
